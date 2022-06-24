5f7b072f807aeb4b8e4e9f04f8edc545f21d67c7
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-06-24 17:51:38 -0500

    New routes for modules

--- IPT-Library-Components/projects/design-system/src/lib/design-system.module.ts
+++ IPT-Library-Components/projects/design-system/src/lib/design-system.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
-import { AsideButtonComponent, ButtonComponent, CheckboxComponent, DatalistComponent, InputComponent, RadioComponent, SelectComponent } from '../public-api';
-
-
-
-
+import { AsideButtonComponent } from './components/aside-button/aside-button.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { DatalistComponent } from './components/datalist/datalist.component';
+import { InputComponent } from './components/input/input.component';
+import { RadioComponent } from './components/radio/radio.component';
+import { SelectComponent } from './components/select/select.component';
 
 @NgModule({
   declarations: [
